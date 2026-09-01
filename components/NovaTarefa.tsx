de600ff88d29ef9cf81df25949cc456e831d9925
Isabelle <isa_landini@hotmail.com>
2026-09-01 17:53:34 -0300

    feat: adiciona formulário de nova tarefa

---
+++ components/NovaTarefa.tsx
@@ -0,0 +1,27 @@
+"use client"; 
+
+import { useState, type FormEvent } from "react";
+
+type NovaTarefaProps = {
+    onAdicionarTarefa: (nome: string) => void
+}
+
+export default function NovaTarefa(props: NovaTarefaProps) {
+    const [nome, setNome] = useState("")
+
+    function adicionarTarefa(e: FormEvent) {
+        e.preventDefault()
+        props.onAdicionarTarefa(nome)
+    }
+    return (
+        <form onSubmit={adicionarTarefa}>
+            <input 
+                value={nome} 
+                onChange={(e) =>
+                   setNome(e.target.value)
+                }
+            />
+            <button>Adicionar</button>
+        </form>
+    )
+}
