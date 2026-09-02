a5f5fbc6f60ffd7f41d4025a9735dc6e0a57a0c9
Isabelle <isa_landini@hotmail.com>
2026-09-02 16:59:27 -0300

    test: adiciona teste do contador de tarefas

---
+++ tests/useContadorDeTarefas.test.ts
@@ -0,0 +1,29 @@
+import { renderHook } from "@testing-library/react";
+import { useContadorDeTarefas } from "@/hooks/useContadorDeTarefas";
+
+const tarefas = [
+    {
+    id: 1,
+    nome: "Estudar Next.js",
+    descricao: "Praticar Server Components",
+    concluida: false
+  },
+  {
+    id: 2,
+    nome: "Estudar TypeScript",
+    descricao: "Praticar tipos",
+    concluida: false
+  },
+  {
+    id: 3,
+    nome: "Estudar Jest",
+    descricao: "Praticar testes",
+    concluida: false
+  }
+]
+
+test("retorna a quantidade de tarefas", () => {
+    const {result} = renderHook(() => useContadorDeTarefas(tarefas));
+
+    expect(result.current).toBe(3)
+});
